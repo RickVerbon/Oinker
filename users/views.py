ca54150524b70ebcf95a35c985312f826c00dd55
Daan <daan_severijns@hotmail.com>
2023-01-11 15:56:57 +0100

    register view authenticate

--- users/views.py
+++ users/views.py
@@ -1,11 +1,22 @@
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .forms import RegisterForm
 
 # Create your views here.
 def register(req):
-    form = RegisterForm()
+    if req.method == 'POST':
+        form = RegisterForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            form = authenticate(username=username, password=raw_password)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
     return render(req, 'users/register.html', {'form': form})
 
 def login(req):
