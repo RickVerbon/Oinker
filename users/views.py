19d5f2006286bf3f94d99ec208ec463cfb58feae
k0wek <rick89@gmail.com>
2023-01-11 15:49:02 +0100

    Added url paths and two basic views with HttpResponse

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def register(req):
+    return HttpResponse("Register page")
+
+def login(req):
+    return HttpResponse("Login page")
