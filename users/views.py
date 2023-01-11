4bbc8581e3c09c9208c742fb8359fd61a1d009fb
k0wek <rick89@gmail.com>
2023-01-11 16:16:51 +0100

    Added form to register.html

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 def register(req):
-    name = "Rick"
-    return render(req, 'users/register.html', {'name': name})
+    form = forms.Register()
+    return render(req, 'users/register.html', {'form': form})
 
 def login(req):
     return HttpResponse("Login page")
