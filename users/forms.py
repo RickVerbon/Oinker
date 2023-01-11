1c5c1749ab7446fac7e511d1c8f9a38a6b5af106
Daan <daan_severijns@hotmail.com>
2023-01-11 15:19:39 +0100

    Password confirm

--- users/forms.py
+++ users/forms.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 
 class Register(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
 
     class Meta:
         model = User
         fields = ('username', 'email')
 
-    def clean_password2(self):
+    def check_password(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords deos not match.')
+            raise forms.ValidationError('Passwords does not match.')
         return cd['password2']
