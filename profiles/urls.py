78ce263a25e32badf18f221fc614cdec1a81143e
k0wek <rick89@gmail.com>
2023-01-13 13:29:08 +0100

    Added follow\unfollow system

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('profile/edit', views.edit_profile, name="edit-profile"),
-    path('profile/<str:username>', views.view_profile, name="view-profile")
+    path('profile/<str:username>', views.view_profile, name="view-profile"),
+    path('profile/<str:username>/follow', views.follow_profile, name="follow-profile"),
+    path('profile/<str:username>/unfollow', views.unfollow_profile, name="unfollow-profile"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -15,7 +15,29 @@ def edit_profile(req):
 
 def view_profile(req, username):
     user = User.objects.get(username=username)
+    logged_in_profile = UserProfile.objects.get(user=req.user)
     user_profile = UserProfile.objects.get(user=user)
     oinks = Oink.get_last_oinks(user=user, amount=4)
     num_oinks = len(oinks)
-    return render(req, "profiles/view_profile.html", {"profile": user_profile, "oinks": oinks, "num_oinks": num_oinks, "user": req.user})
+    return render(req, "profiles/view_profile.html", {"profile": user_profile, "oinks": oinks, "num_oinks": num_oinks, "user": req.user, "logged_in_profile": logged_in_profile})
+
+
+def follow_profile(req, username):
+    logged_in_user = req.user
+    logged_in_profile = UserProfile.objects.get(user=logged_in_user)
+    user = User.objects.get(username=username)
+    profile = UserProfile.objects.get(user=user)
+    if not logged_in_profile.following.filter(pk=profile.pk).exists():
+        logged_in_profile.following.add(profile)
+    return redirect("home")
+
+
+def unfollow_profile(req, username):
+    logged_in_user = req.user
+    logged_in_profile = UserProfile.objects.get(user=logged_in_user)
+    user = User.objects.get(username=username)
+    profile = UserProfile.objects.get(user=user)
+    if logged_in_profile.following.filter(pk=profile.pk).exists():
+        logged_in_profile.following.remove(profile)
+    return redirect("home")
+
